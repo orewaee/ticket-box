6857d3682507690e60a9febddf442920a2e45867
orewaee <orewaee@gmail.com>
2024-08-12 15:14:11 +0400

    feat(frontend): loader added

--- web/pages/dashboard/index.vue
+++ web/pages/dashboard/index.vue
@@ -16,7 +16,13 @@ onMounted(async () => {
     <MyHeader :guild="null" :avatar-url="userState.avatarUrl" />
     <div class="container">
       <h1>Dashboard</h1>
-      <div class="guilds">
+      <div class="guilds-skeleton" v-if="guildsState.loading">
+        <div class="item" />
+        <div class="item" />
+        <div class="item" />
+      </div>
+
+      <div class="guilds" v-else>
         <NuxtLink class="link" v-for="guild in guildsState.guilds as Guild[]" :to="'/dashboard/' + guild.id">
           <Guild :id="guild.id" :with-bot="guild.with_bot" :name="guild.name" :icon-url="guild.icon_url" />
         </NuxtLink>
@@ -39,6 +45,34 @@ onMounted(async () => {
 
     margin: 0 auto;
     padding: 24px;
+
+    .guilds-skeleton {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(408px, 1fr));
+      grid-gap: 24px;
+
+      .item {
+        height: 96px;
+
+        background: linear-gradient(
+          135deg,
+          var(--background-300),
+          var(--background-400),
+          var(--background-300)
+        );
+
+        background-size: 200% 100%;
+        border-radius: 24px;
+
+        animation: 2s shine linear infinite;
+      }
+
+      @keyframes shine {
+        to {
+          background-position-x: -200%;
+        }
+      }
+    }
 
     h1 {
       color: var(--foreground-500);

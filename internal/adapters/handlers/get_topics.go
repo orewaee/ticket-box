73d2c86e5a2375840c57265837fe945363181a58
orewaee <orewaee@gmail.com>
2024-08-12 13:05:51 +0400

    feat(backend): handlers have been added

---
+++ internal/adapters/handlers/get_topics.go
@@ -0,0 +1,78 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/orewaee/ticket-box/internal/app/ports"
+	"github.com/orewaee/ticket-box/internal/dto"
+	"github.com/orewaee/ticket-box/internal/utils"
+	"net/http"
+	"strings"
+)
+
+type GetTopicsHandler struct {
+	topicService   ports.TopicService
+	discordService ports.DiscordService
+}
+
+func NewGetTopicsHandler(topicService ports.TopicService, discordService ports.DiscordService) *GetTopicsHandler {
+	return &GetTopicsHandler{
+		topicService:   topicService,
+		discordService: discordService,
+	}
+}
+
+// GET /topics/{guild_id}
+func (handler *GetTopicsHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+	guildId := request.PathValue("guild_id")
+	if guildId == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		utils.WriteString(writer, "missing guild_id")
+		return
+	}
+
+	authorization := request.Header.Get("Authorization")
+	if authorization == "" {
+		writer.WriteHeader(http.StatusUnauthorized)
+		utils.WriteString(writer, "missing token")
+		return
+	}
+
+	accessToken := strings.TrimPrefix(authorization, "Bearer ")
+	isAdmin := handler.discordService.CurrentUserIsGuildAdmin(accessToken, guildId)
+
+	if !isAdmin {
+		writer.WriteHeader(http.StatusUnauthorized)
+		utils.WriteString(writer, "you can't get the list of topics of this guild")
+		return
+	}
+
+	topics, err := handler.topicService.GetTopicsByGuildId(guildId)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		utils.WriteString(writer, "error getting list of topics: "+err.Error())
+		return
+	}
+
+	data := make([]*dto.Topic, len(topics))
+	for i, topic := range topics {
+		data[i] = &dto.Topic{
+			Id:          topic.Id,
+			GuildId:     topic.GuildId,
+			Emoji:       topic.Emoji,
+			Name:        topic.Name,
+			Description: topic.Description,
+		}
+	}
+
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		utils.WriteString(writer, "error getting list of topics: "+err.Error())
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	utils.WriteBytes(writer, bytes)
+}
---
+++ internal/adapters/handlers/get_user.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com/orewaee/ticket-box/internal/app/ports"
+	"github.com/orewaee/ticket-box/internal/dto"
+	"github.com/orewaee/ticket-box/internal/utils"
+	"net/http"
+)
+
+type GetUserHandler struct {
+	discordService ports.DiscordService
+}
+
+func NewGetUserHandler(discordService ports.DiscordService) *GetUserHandler {
+	return &GetUserHandler{
+		discordService: discordService,
+	}
+}
+
+// GET /user
+func (handler *GetUserHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+
+	id := request.Context().Value("id").(string)
+	accessToken := request.Context().Value("accessToken").(string)
+
+	currentUser, err := handler.discordService.GetCurrentUser(accessToken)
+	if err != nil {
+		writer.WriteHeader(http.StatusUnauthorized)
+		utils.WriteString(writer, err.Error())
+		return
+	}
+
+	if currentUser.Id != id {
+		writer.WriteHeader(http.StatusUnauthorized)
+		utils.WriteString(writer, "identifier mismatch")
+		return
+	}
+
+	user := &dto.User{
+		Id:        currentUser.Id,
+		Username:  currentUser.Username,
+		AvatarUrl: currentUser.AvatarUrl,
+	}
+
+	bytes, err := json.Marshal(user)
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	utils.WriteBytes(writer, bytes)
+}

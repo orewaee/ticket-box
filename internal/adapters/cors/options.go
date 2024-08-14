b4adaad459f6b64c9270178bb14b734d53a2fbcd
orewaee <orewaee@gmail.com>
2024-08-14 12:25:56 +0400

    fix(backend): delete method added to allowed cors methods

--- internal/adapters/cors/options.go
+++ internal/adapters/cors/options.go
@@ -10,7 +10,7 @@ func NewOptionsHandler() *OptionsHandler {
 
 func (handler *OptionsHandler) ServeHTTP(writer http.ResponseWriter, _ *http.Request) {
 	writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 	writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, User-agent")
 	writer.WriteHeader(http.StatusOK)
 }
